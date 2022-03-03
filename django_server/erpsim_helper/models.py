c8ef624b2d227c8bb89afe9d2446ac8b176f2f11
romainmeunier93 <65892038+romainmeunier93@users.noreply.github.com>
2022-03-03 16:19:21 +0100

    admin interface finished

--- django_server/erpsim_helper/models.py
+++ django_server/erpsim_helper/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from .tasks import get_game_latest_data
 
+import datetime
+
 # Create your models here.
 
 class Game(models.Model):
@@ -14,3 +16,31 @@ class Game(models.Model):
     def save(self, *args, **kwargs):
         super(Game, self).save(*args, **kwargs)
         #get_game_latest_data(self.id, self.odata_flow, self.game_set, self.team)
+
+    def __str__(self):
+        return f"Game : {str(self.id).rjust(3, '0')} - {datetime.datetime.strptime(str(self.creation_date), '%Y-%m-%d %H:%M:%S+00:00').strftime('%d/%m/%Y %H:%M')}"
+
+class CompanyValuation(models.Model):
+    id_company_valuation = models.BigAutoField(primary_key=True)
+    row_number = models.IntegerField()
+    company_code = models.CharField(max_length=2)
+    sim_round = models.IntegerField()
+    sim_step = models.IntegerField()
+    sim_calendar_date = models.DateTimeField()
+    sim_period = models.IntegerField()
+    sim_elapsed_steps = models.IntegerField()
+    bank_cash_account = models.FloatField()
+    accounts_receivable = models.IntegerField()
+    bank_loan = models.FloatField()
+    accounts_payable = models.FloatField()
+    profit = models.FloatField()
+    debt_loading = models.FloatField()
+    credit_rating = models.CharField(max_length=10)
+    company_risk_rate_pct = models.FloatField()
+    company_valuation = models.FloatField()
+    currency = models.CharField(max_length=3)
+    id_game = models.ForeignKey(Game, models.CASCADE, db_column='id_game')
+
+    class Meta:
+        managed = False
+        db_table = 'company_valuation'
