30fba6eecb8446779017e133a641395974bf8a04
Jean-Charles Coudon <65890983+RealAcharnes@users.noreply.github.com>
2022-05-16 17:04:33 +0200

    start tests for django plotly dash

---
+++ ERPsimHelper/ERPgames/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ErpgamesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ERPgames'
---
+++ ERPsimHelper/ERPgames/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from django.views.generic import RedirectView
+from . import views
+
+urlpatterns = [
+    path('django_plotly_dash/', include('django_plotly_dash.urls'))
+    # path('', RedirectView.as_view(url='form')),
+    # path('form', views.form, name='form'),
+    # path('detail', views.detail, name='detail')
+]
